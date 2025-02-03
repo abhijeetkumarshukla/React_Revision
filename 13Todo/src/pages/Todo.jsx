2a341041f897134bfded83abfc7d6edb747db9cc
abhijeetkumarshukla <shuklaabhijeet510@gmail.com>
2025-02-03 23:07:22 +0530

    done

---
+++ 13Todo/src/pages/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTodo = () => {
+    if (!task) return;
+    setTodos([...todos, task]);
+    setTask("");
+  };
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const editTodo = (index) => {
+    setTask(todos[index]);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={task}
+          placeholder="Enter a task"
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <span> {todo}</span>
+            <div>
+              <button onClick={() => editTodo(index)}>Edit Task</button>
+              <button onClick={() => deleteTodo(index)}>Delete Task</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
